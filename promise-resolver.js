1276c8be6583c4b268ad770df65c34a62ea9cdc8
shau05 <85293036+shau05@users.noreply.github.com>
2021-06-05 00:13:24 +0530

    Update promise-resolver.js

--- promise-resolver.js
+++ promise-resolver.js
@@ -1,1 +1,12 @@
 
+export class PromiseResolver {
+  constructor() {
+    this.promise_ = new Promise((resolve, reject) => {
+      this.resolve_ = resolve;
+      this.reject_ = reject;
+    });
+  }
+  get promise() { return this.promise_ }
+  get resolve() { return this.resolve_ }
+  get reject() { return this.reject_ }
+}
