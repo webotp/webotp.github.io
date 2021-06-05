8948b68b67925c684dafba662a8c8d7f77cb1a81
shau05 <85293036+shau05@users.noreply.github.com>
2021-06-05 22:34:36 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,5 +7,5 @@ self.addEventListener('paymentrequest', e => {
 
 self.addEventListener('canmakepayment', e => {
   console.log("can make payment");
-  e.respondWith("connection");
+  e.respondWith(true);
 });
