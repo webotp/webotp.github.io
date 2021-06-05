aeb378eaee4fb4e2d7e23b07f3a5efe698ca6413
shau05 <85293036+shau05@users.noreply.github.com>
2021-06-05 16:31:51 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
 
 
 self.addEventListener('paymentrequest', e => {
+  console.log("requested");
   e.openWindow("https://shau05.github.io");
 });
 
 self.addEventListener('canmakepayment', e => {
+  console.log("can make payment");
   e.respondWith(true);
 });
