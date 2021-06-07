805fb7d5ac5282df72ecd0530abf5ed6bb2cdd02
shau05 <85293036+shau05@users.noreply.github.com>
2021-06-07 12:39:47 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js');
+  registration = await navigator.serviceWorker.ready;
+    if (!registration.paymentManager) {
+      return;
+    }
+    await registration.paymentManager.instruments.set(
+      "instrument-id-1",
+      {
+        name: 'Payment Method 1',
+        method: ["https://shau05.github.io"]
+      }
+    );
 }
