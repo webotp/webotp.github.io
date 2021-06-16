90ed05113ba1b045e16350e14d5caced3117b5da
Sujeet Kushwaha <sujeet.intern@PP-C02FD3B4MD6M.local>
2021-06-16 12:18:54 +0530

    changes

--- sw.js
+++ sw.js
@@ -1,78 +1,3 @@
-// class PromiseResolver {
-//   constructor() {
-//     this.promise_ = new Promise((resolve, reject) => {
-//       this.resolve_ = resolve;
-//       this.reject_ = reject;
-//     });
-//   }
-//   get promise() {
-//     return this.promise_
-//   }
-//   get resolve() {
-//     return this.resolve_
-//   }
-//   get reject() {
-//     return this.reject_
-//   }
-// }
-// let cc;
-
-// class FrontendController {
-//   constructor(event) {
-//     this.resolver = new PromiseResolver();
-//     this.pre = event;
-//     this.pre.respondWith(this.resolver.promise);
-//   }
-//   async appendClient(client) {
-//     this.client = client;
-//     this.client.postMessage({
-//       msg: "amount",
-//       total: this.pre.total
-//     });
-//   }
-//   async authorize(paymentHandlerResponse) {
-//     this.resolver.resolve(paymentHandlerResponse);
-//   }
-//   async success(text) {
-//     return this.resolver.resolve(text);
-//   }
-//   async cancel(text) {
-//     return this.resolver.reject(text);
-//   }
-// }
-
-// self.addEventListener('message', async e => {
-//   switch (e.data.msg) {
-//     case "cancel":
-//       await cc.cancel(e.data.contents);
-//       cc = null;
-//       break;
-//     case "success":
-//       await cc.success(e.data.contents);
-//       cc = null;
-//       break;
-//     case "failed":
-//       await cc.success(e.data.contents);
-//       cc = null;
-//       break;
-//     case "client_ready":
-//       cc.appendClient(e.source);
-//       break;
-//   }
-// })
-
-// self.addEventListener('paymentrequest', e => {
-//   // e.methodData[0].data.url
-//   console.log(e);
-//   cc = new FrontendController(e);
-//   e.openWindow("https://shau05.github.io");
-// });
-
-// self.addEventListener('canmakepayment', e => {
-//   console.log(e);
-//   e.respondWith(true);
-// });
-
 class PromiseResolver {
   constructor() {
     this.promise_ = new Promise((resolve, reject) => {
@@ -81,13 +6,13 @@ class PromiseResolver {
     });
   }
   get promise() {
-    return this.promise_;
+    return this.promise_
   }
   get resolve() {
-    return this.resolve_;
+    return this.resolve_
   }
   get reject() {
-    return this.reject_;
+    return this.reject_
   }
 }
 let cc;
@@ -100,15 +25,15 @@ class FrontendController {
   }
   async appendClient(client) {
     this.client = client;
-    // this.client.postMessage({
-    //     msg: "amount",
-    //     total: this.pre.total
-    // });
+    this.client.postMessage({
+      msg: "amount",
+      total: this.pre.total
+    });
   }
   async authorize(paymentHandlerResponse) {
     this.resolver.resolve(paymentHandlerResponse);
   }
-  async status(text) {
+  async success(text) {
     return this.resolver.resolve(text);
   }
   async cancel(text) {
@@ -116,50 +41,36 @@ class FrontendController {
   }
 }
 
-let response = (message) => {
-  return {
-    methodName: cc.pre.methodData[0].supportedMethods,
-    details: {
-      message: message,
-    }
-  }
-}
-
-self.addEventListener("message", async e => {
-  console.log(e);
-  switch (e.data.message_type) {
-    case "status":
-      const result = response(e.data.message);
-      await cc.status(result);
+self.addEventListener('message', async e => {
+  switch (e.data.msg) {
+    case "cancel":
+      await cc.cancel(e.data.contents);
       cc = null;
       break;
-    // default:
-    //   const result = response(e.data.message);
-    //   await cc.cancel(result);
-    //   cc = null;
-    //   break;
+    case "success":
+      await cc.success(e.data.contents);
+      cc = null;
+      break;
+    case "failed":
+      await cc.success(e.data.contents);
+      cc = null;
+      break;
+    case "client_ready":
+      cc.appendClient(e.source);
+      break;
   }
-});
+})
 
-self.addEventListener("paymentrequest", e => {
-  // let redirectUrl = e.methodData[0].data.url;
+self.addEventListener('paymentrequest', e => {
+  // e.methodData[0].data.url
+  console.log(e);
   cc = new FrontendController(e);
-  // check if client window is not null.
-  e
-    .openWindow("https://shau05.github.io")
-    .then(client => {
-      if (client == null) {
-        cc.cancel("failed to open the window");
-      } else {
-        cc.appendClient(client);
-      }
-    })
-    .catch(err => {
-      console.log(err);
-    });
+  e.openWindow("https://shau05.github.io");
 });
 
-self.addEventListener("canmakepayment", e => {
+self.addEventListener('canmakepayment', e => {
   console.log(e);
   e.respondWith(true);
 });
+
+
